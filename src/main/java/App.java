e10f071c6be9513a6aa4e1d734c387152139b967
eqqbq <160983984+eqqbq@users.noreply.github.com>
2024-04-26 15:22:17 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,7 @@ public class App {
         do {
             switch (opcion){
                 case 1:
+                    System.out.println("Opción 1");
                     break;
                 case 2:
                     break;
