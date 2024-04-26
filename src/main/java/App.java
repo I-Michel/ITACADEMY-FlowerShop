9304ea57d476f803f045f66a5f5bded4f1a321e1
eqqbq <raizen580@gmail.com>
2024-04-26 15:37:43 +0200

    Commit prueba ide

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,7 @@ public class App {
             switch (opcion){
                 case 1:
                     System.out.println("Opción número 1.");
+                    //prueba 2
                     break;
                 case 2:
                     break;
