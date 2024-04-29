0e6511174dea4edf31a19c6a6d9b7456c778dbf2
dortegva <ortegavalero.daniel@gmail.com>
2024-04-29 13:20:24 +0200

    Ticket

--- src/main/java/Ticket/Ticket.java
+++ src/main/java/Ticket/Ticket.java
@@ -1,4 +1,31 @@
 package Ticket;
 
+import Product.Product;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+//Serializar
 public class Ticket {
+    private LocalDateTime dateTime;
+    private float price;
+    private ArrayList<Product> productList;
+
+    public Ticket(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+        productList =  new ArrayList<Product>();
+        price=calculateTotalPrice();
+    }
+    private float calculateTotalPrice(){
+        price=0;
+        for (Product producto:productList){
+           price=price+producto.getPrice();
+        }
+        return price;
+    }
+    private addProductTicket(){};
+    private removeProductTicket(){};
+
 }
+
+
