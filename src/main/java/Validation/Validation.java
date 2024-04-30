59230eaef762a9814e58b6ae0de051856b241b27
eqqbq <raizen580@gmail.com>
2024-04-30 17:00:13 +0200

    Menu/Validate int update

--- src/main/java/Validation/Validation.java
+++ src/main/java/Validation/Validation.java
@@ -3,37 +3,42 @@ package Validation;
 import Product.Product;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Validation {
 
-    public static void isStockEmpty(ArrayList<Product> productList){
-        if (productList.isEmpty()){
+    public static void isStockEmpty(ArrayList<Product> productList) {
+        if (productList.isEmpty()) {
             System.out.println("Stock you are trying to use is empty.");
         }
     }
 
-    public static void enoughStock(){
+    public static void enoughStock() {
+
     }
 
-    public static void validateInt(){
+    public static int validateInt(String message) {
         boolean ok = false;
         Scanner sc = new Scanner(System.in);
+        int resultado = 0;
 
         do {
             System.out.println(message);
             try {
-                sc.nextInt();
+                resultado = sc.nextInt();
                 ok = true;
-            } catch (Exception e) {
+            } catch (InputMismatchException e) {
                 System.out.println(e);
                 sc.nextLine();
             }
         } while (!ok);
+
+        return resultado;
     }
 
-    public static void validateText(String color){
-        if (color.matches("[0-9]+")){
+    public static void validateText(String color) {
+        if (color.matches("[0-9]+")) {
             System.out.println("Expected a color instead of numbers");
         }
     }
