b2053ea8c1e6e836245d0e1e3682b42e8c8b4ab6
Michel <146105710+I-Michel@users.noreply.github.com>
2024-05-02 13:13:46 +0200

    Cambios enum + menu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,13 +1,15 @@
 import Product.*;
+import Product.Decoration.Material;
 
 import java.util.HashMap;
 
 import static Validation.Validation.validateInt;
+import static Validation.Validation.validateText;
 
 public class Menu {
-    static HashMap<Flower, Integer> flowerList = new HashMap<>();
-    static HashMap<Tree, Integer> treeList = new HashMap<>();
-    static HashMap<Decoration, Integer> decorationList = new HashMap<>();
+    static HashMap<Flower, Integer> flowerStock = new HashMap<>();
+    static HashMap<Tree, Integer> treeStock = new HashMap<>();
+    static HashMap<Decoration, Integer> decorationStock = new HashMap<>();
 
 
     public static void start() {
@@ -24,7 +26,7 @@ public class Menu {
                     //createFlowerShop();
                     break;
                 case 2:
-                    addProductStock();
+                    addProduct();
                     break;
                 case 3:
                     //removeProductStock();
@@ -59,54 +61,87 @@ public class Menu {
                     //readJSON();
                     break;
                 default:
-                    System.out.println("Please choose one of the options.");
+                    System.out.println("Please choose a valid option.");
             }
         } while (option != 0);
     }
 
-    public static void addProductStock() {
+    public static void addProduct() {
         int option = 0;
         int type = 0;
 
         do {
-            type = validateInt("What type of product would you like to add? Please choose an option:\n1. Flower.\n 2. Tree. \n3. Decoration");
+            type = validateInt("What type of product would you like to add?" +
+                    "\n1. Flower.\n 2. Tree. \n3. Decoration");
             if (type < 1 || type > 3) {
-                System.out.println("Invalid option.");
+                System.out.println("Please choose a valid option.");
             }
         } while (type < 1 || type > 3);
 
         do {
-            option = validateInt("Would you like to add an existing product or a new one? Please choose an option:\n1. Existing product.\n 2. New product.");
-            switch (option) {
-                case 1: // Existing
-
-                    break;
-                case 2: // New product
-                    if (type == 1) {
-                        flowerList.put(ProductFactory.createFlower(price, color), units);
-                    } else if (type == 2) {
-                        treeList.put(ProductFactory.createTree(price, height), units);
-                    } else if (type == 3) {
-                        decorationList.put(ProductFactory.createDecoration(price, material), units);                    }
-                    break;
-                default:
-                    System.out.println("Invalid option.");
+            option = validateInt("Is it an existing product or a new one?" +
+                    "\n1. Existing product.\n 2. New product.");
+            if (option < 1 || option > 2) {
+                System.out.println("Please choose a valid option.");
             }
         } while (option != 1 && option != 2);
 
-        int units = validateInt("How many units do you want to add?");
-
+        int units = validateInt("How many do you want to add?");
 
         switch (option) {
             case 1:
-                System.out.println("How many do you want to add?");
+                addStock(type, units);
                 break;
             case 2:
-                break;
-            case 3:
+                createNewProduct(type, units);
                 break;
             default:
-                System.out.println("Please choose one of the options.");
+                System.out.println("Please choose a valid option.");
+        }
+    }
+
+    public static void addStock(int type, int units) {
+        switch (type) {
+            case 1: // Flower
+
+                break;
+            case 2: // Tree
+
+                break;
+            case 3: // Decoration
+
+                break;
+        }
+    }
+
+    public static void createNewProduct(int type, int units) {
+        float price = validateFloat("Which is the price of the new product?");
+
+        switch (type) {
+            case 1:
+                String color = validateText("Which is the color of the new flower?");
+                flowerStock.put(ProductFactory.createFlower(price, color), units);
+                break;
+            case 2:
+                float height = validateFloat("Which is the height of the new tree?");
+                treeStock.put(ProductFactory.createTree(price, height), units);
+                break;
+            case 3:
+                int numMaterial = 0;
+                Material material = null;
+                do {
+                    numMaterial = validateInt("Which is the material of the new decoration? Please choose an option:" +
+                            "\n1. Wood.\n 2. Plastic");
+                    if (numMaterial == 1) {
+                        material = Material.WOOD;
+                    } else if (numMaterial == 2) {
+                        material = Material.PLASTIC;
+                    } else {
+                        System.out.println("Invalid option.");
+                    }
+                } while (numMaterial != 1 && numMaterial != 2);
+                decorationStock.put(ProductFactory.createDecoration(price, material), units);
+                break;
         }
     }
 }
