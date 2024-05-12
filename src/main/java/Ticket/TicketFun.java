007a31d969b6e2a0b608124335d9a6166eef7ed0
dortegva <ortegavalero.daniel@gmail.com>
2024-05-12 20:19:19 +0200

    Ticket

--- src/main/java/Ticket/TicketFun.java
+++ src/main/java/Ticket/TicketFun.java
@@ -17,43 +17,12 @@ public class TicketFun {
 
         ResultSet rs = stmt.getGeneratedKeys();
         stmt.executeUpdate();
-        if (rs.next()) {
-            int ticketId = rs.getInt(1);
-            ticket.setTicketID(ticketId);
 
-            Map<Product, Integer> products = ticket.getProducts();
-
-            for (Map.Entry<Product, Integer> entry : products.entrySet()) {
-                Product product = entry.getKey();
-                Integer quantity = entry.getValue();
-
-                PreparedStatement statementProductTicket = connection.prepareStatement(QueriesSQL.SQL_INSERT_PRODUCT_TICKET);
-                statementProductTicket.setInt(1, ticketId);
-                statementProductTicket.setInt(2, product.getProductId());
-                statementProductTicket.setInt(3, quantity);
-                statementProductTicket.executeUpdate();
             }
-        }
-    }
+
+
 
     catch (SQLException e) {
         System.err.println("Falta escribir mensaje error");
         System.err.printf(e.getMessage());
-    }}
-    public static void pruebaConnection() {
-
-        try {
-            Connection con = MySQLDB.connect();
-            float i=0;
-            PreparedStatement stmt = con.prepareStatement(
-                    "INSERT INTO product (price, stock, type ) " +
-                            "VALUES ("+i+", 1, 'berenjena')");
-
-
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println("Falta escribir mensaje error");
-            System.err.printf(e.getMessage());
-        }
-        System.out.println("Se ha ejecutado");
-}
+    }}}
